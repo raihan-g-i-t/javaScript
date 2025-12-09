a7efa20e3a708d643d5d0da99300ce4c4a92503e
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-12-09 20:00:30 +0600

    js update

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,4 +1,13 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
-let array = [[1,2,3], ["red", "green", "yellow"]];
+let winPatterns = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 3, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+]
