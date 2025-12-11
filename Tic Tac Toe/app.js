db21b5f40d30b87fe6d584e8761bb5169f92e23d
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-12-11 20:19:35 +0600

    js update

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,5 +1,6 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let turnO = true;
 
 let winPatterns = [
     [0, 1, 2],
@@ -10,4 +11,22 @@ let winPatterns = [
     [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
-]
+];
+
+boxes.forEach((box) =>{
+    box.addEventListener("click", () => {
+        if(turnO){
+            box.innerText = "o";
+            turnO = false;
+        }else{
+            box.innerText = "x";
+            turnO = true;
+        }
+    });
+});
+
+resetBtn.addEventListener("click", ()=>{
+    boxes.forEach((box) => {
+        box.innerText = null;
+    });
+});
