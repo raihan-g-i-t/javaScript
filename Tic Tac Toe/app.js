a6c293614ffc6ea3ac3379c7330413d6430cfe0a
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-12-15 18:04:38 +0600

    Finished

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,38 +1,69 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let msg = document.querySelector("#msg");
 let turnO = true;
+let gameOver = false;
 
 let winPatterns = [
     [0, 1, 2],
     [0, 4, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 3, 6],
+    [2, 4, 6],
     [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
 ];
 
-boxes.forEach((box) =>{
+boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if(turnO){
-            box.innerText = "o";
+        if (box.innerText !== "" || gameOver) return;
+
+        if (turnO) {
+            box.innerText = "O";
             turnO = false;
-        }else{
-            box.innerText = "x";
+        } else {
+            box.innerText = "X";
             turnO = true;
         }
+
         box.disabled = true;
+        checkWinner();
     });
-
-    checkWinner = () => {
-
-    }
 });
 
-resetBtn.addEventListener("click", ()=>{
-    boxes.forEach((box) => {
-        box.innerText = null;
-        box.disabled = false;
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
+            highlightWinner(pattern);
+            msg.innerText = `Congratulations! The winner is ${pos1}`;
+            gameOver = true;
+            return;
+        }
+    }
+};
+
+const highlightWinner = (pattern) => {
+    pattern.forEach((index) => {
+        boxes[index].classList.add("win");
     });
+
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+}
+
+resetBtn.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+        box.classList.remove("win");
+    });
+    msg.innerText = "";
+    turnO = true;
+    gameOver = false;
 });
