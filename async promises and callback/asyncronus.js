22371fe5fc0abc1bfb3344fdba771902a7288134
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-12-23 19:34:19 +0600

    initial APC

---
+++ async promises and callback/asyncronus.js
@@ -0,0 +1,14 @@
+console.log("1");
+console.log("2");
+
+// function hello(){
+//     console.log("Hello");
+// }
+// setTimeout(hello, 4000);
+
+setTimeout(() => {
+    console.log("Hello");
+}, 4000);
+
+console.log("3");
+console.log("4");
