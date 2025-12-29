381d4f4ace299cf5c769caae6734038289a770c6
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-12-29 18:33:59 +0600

    promise

--- async promises and callback/app.js
+++ async promises and callback/app.js
@@ -12,6 +12,38 @@
 //     getdata(3);
 // });
 
-let promise = new Promise((resolve, reject) => {
+// let promise = new Promise((resolve, reject) => {
+//     console.log("I am a promise");
+//     resolve("success");
+// });
+
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
     console.log("I am a promise");
+    // resolve("success");
+    reject("Error occured")
+    });
+}
+
+let promise = getPromise();
+promise.then((res) => {
+    console.log("sucess", res);
+});
+
+promise.catch((err) => {
+    console.log("errpr: ", err);
+});
+
+let asyncFunc = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("timeout");
+            resolve("success");
+        }, 2000);
+    });
+}
+
+let a1 = asyncFunc();
+a1.then((res) => {
+    console.log(res);
 });
