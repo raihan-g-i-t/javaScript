f82b3cffe996a087081017ef0435055d6b30516e
fil303 <webfil303@gmail.com>
2025-12-30 20:09:03 +0600

    complete APC

--- async promises and callback/app.js
+++ async promises and callback/app.js
@@ -21,13 +21,13 @@ const getPromise = () => {
     return new Promise((resolve, reject) => {
     console.log("I am a promise");
     // resolve("success");
-    reject("Error occured")
+    reject("Error occur");
     });
 }
 
 let promise = getPromise();
 promise.then((res) => {
-    console.log("sucess", res);
+    console.log("success", res);
 });
 
 promise.catch((err) => {
@@ -47,3 +47,27 @@ let a1 = asyncFunc();
 a1.then((res) => {
     console.log(res);
 });
+
+//async
+
+let getData2 = (data) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(200);
+            console.log("data: ", data)
+        }, 2000)
+    })
+}
+
+async function getAllData() {
+    console.log("getting data 1");
+    await getData2(1);
+    console.log("getting data 2");
+    await getData2(2);
+    console.log("getting data 3");
+    await getData2(3);
+    console.log("getting data 4");
+    await getData2(4);
+    console.log("getting data 5");
+    await getData2(5);
+}
