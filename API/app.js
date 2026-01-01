8ac1f7cd8ac2474f84029a1531618ec74c4f603e
fil303 <webfil303@gmail.com>
2026-01-01 20:03:23 +0600

    Inital APIs

---
+++ API/app.js
@@ -0,0 +1,14 @@
+let URL = "https://official-joke-api.appspot.com/random_joke";
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("#btn");
+
+const getJokes = async () => {
+    let response = await fetch(URL);
+    // console.log(response.json());
+    let data = await response.json();
+    // console.log(data);
+    jokes.innerText = `${data.setup} ${data.punchline}`;
+};
+
+button.addEventListener("click", getJokes);
+// button.addEventListener("click", getJokes);
